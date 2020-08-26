9914f41d07011f7166adc9930465b87c78740046
intombia <67989029+intombia@users.noreply.github.com>
2020-08-26 07:49:50 +0300

    Add files via upload

---
+++ api/api.php
@@ -0,0 +1,16 @@
+<?php
+	/*$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+
+	$dbh = new PDO('mysql:host=localhost;dbname=cy70845_db', 'cy70845_db', 'cy70845_db');
+
+	$q = "INSERT INTO orders(`time`, name, phone, email)
+		VALUES(NOW(), :name, :phone, :email)";
+	$sth = $dbh->prepare($q);
+	$sth->bindParam(':name', $name, PDO::PARAM_STR);
+	$sth->bindParam(':phone', $phone, PDO::PARAM_STR);
+	$sth->bindParam(':email', $email, PDO::PARAM_STR);
+	$sth->execute();*/
+
+	echo "1";
